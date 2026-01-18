a3829625a9e2a56683398cfc47cc9febe6025727
mecyahh <mecyah@opfgrp.com>
2026-01-18 12:58:42 -0500

    Update supabaseAdmin.ts

--- lib/supabaseAdmin.ts
+++ lib/supabaseAdmin.ts
@@ -1,11 +1,8 @@
 // /lib/supabaseAdmin.ts
 import { createClient } from '@supabase/supabase-js'
 
-const url = process.env.NEXT_PUBLIC_SUPABASE_URL
-const serviceRole = process.env.SUPABASE_SERVICE_ROLE_KEY
-
-if (!url) throw new Error('Missing env NEXT_PUBLIC_SUPABASE_URL')
-if (!serviceRole) throw new Error('Missing env SUPABASE_SERVICE_ROLE_KEY')
+const url = process.env.NEXT_PUBLIC_SUPABASE_URL!
+const serviceRole = process.env.SUPABASE_SERVICE_ROLE_KEY!
 
 export const supabaseAdmin = createClient(url, serviceRole, {
   auth: {
