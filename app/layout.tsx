1dbd4647ddcc6d3fe6a87ba55a29ea46dd799727
mecyahh <mecyah@opfgrp.com>
2026-01-18 12:28:49 -0500

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,18 +1,17 @@
 import './globals.css'
+import ThemeProvider from './components/ThemeProvider'
 
 export const metadata = {
   title: 'Flow',
-  description: 'Everything is flowing.',
+  description: 'Deal tracking',
 }
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode
-}) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        <ThemeProvider>{children}</ThemeProvider>
+      </body>
     </html>
   )
 }
